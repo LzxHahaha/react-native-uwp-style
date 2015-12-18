da4be5e48b486d8175588dd72e755869d4d79122
LzxHahaha <lzxglhf@live.com>
2015-12-19 00:56:16 +0800

    Add CommandBar component

---
+++ js/components/navigator/CommandBar.js
@@ -0,0 +1,113 @@
+import React, {
+    Animated,
+    Component,
+    PropTypes,
+    Text,
+    TouchableOpacity,
+    View,
+    Image
+} from 'react-native';
+
+import {styles} from './CommandBar.style';
+import {SCREEN_WIDTH} from '../../global.style';
+
+propTypes = {};
+
+defaultProps = {};
+
+export default class CommandBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+            barHeight: new Animated.Value(50),
+            textOpacity: new Animated.Value(0)
+        };
+    }
+
+    toggle() {
+        const {isOpen, barHeight, textOpacity} = this.state;
+        let needOpen = !isOpen;
+
+        if (needOpen) {
+            Animated.parallel([
+                Animated.timing(barHeight, {
+                    toValue: 65,
+                    duration: 300
+                }),
+                Animated.timing(textOpacity, {
+                    toValue: 1,
+                    duration: 50
+                })
+            ]).start();
+        }
+        else {
+            Animated.parallel([
+                Animated.timing(barHeight, {
+                    toValue: 50,
+                    duration: 300
+                }),
+                Animated.timing(textOpacity, {
+                    toValue: 0,
+                    duration: 50
+                })
+            ]).start();
+        }
+
+        this.setState({isOpen: needOpen});
+    }
+
+    renderButtons() {
+        const {barButtons} = this.props;
+
+        let buttons = [];
+
+        if (barButtons) {
+            let max = SCREEN_WIDTH / 65 - 1;
+
+            for (let i = 0; i < barButtons.length && i < max; ++i) {
+                const {icon, text, onPress} = barButtons[i];
+
+                buttons.push(
+                    <TouchableOpacity onPress={onPress && onPress()}>
+                        <View style={styles.iconButton}>
+                            <View style={styles.iconView}>
+                                {
+                                    icon &&
+                                    (<Image source={icon} style={styles.icon}/>)
+                                }
+                            </View>
+                            <Animated.Text style={[styles.iconText, {opacity: this.state.textOpacity}]}>
+                                {text}
+                            </Animated.Text>
+                        </View>
+                    </TouchableOpacity>
+                );
+            }
+        }
+
+        buttons.push(
+            <TouchableOpacity onPress={this.toggle.bind(this)}>
+                <View style={[styles.moreButton]}>
+                    <Text style={styles.moreText}>. . .</Text>
+                </View>
+            </TouchableOpacity>
+        );
+
+        return buttons;
+    }
+
+    render() {
+        const {backgroundColor} = this.props;
+
+        return (
+            <Animated.View style={[styles.container, {height: this.state.barHeight}]}>
+                {this.renderButtons()}
+            </Animated.View>
+        );
+    }
+}
+
+CommandBar.propTypes = propTypes;
+CommandBar.defaultProps = defaultProps;
---
+++ js/components/navigator/CommandBar.style.js
@@ -0,0 +1,60 @@
+import {
+    StyleSheet
+} from 'react-native';
+
+import {GlobalFontSize} from '../../global.style';
+
+export var styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-end',
+        backgroundColor: 'lightGray'
+    },
+
+    iconButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 65,
+        height: 65
+    },
+
+    iconView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        paddingTop: -10
+    },
+
+    icon: {
+        width: 20,
+        resizeMode: 'contain'
+    },
+
+    iconText: {
+        color: 'black',
+        fontSize: GlobalFontSize.Caption,
+        textAlign: 'center',
+        marginTop: -15
+    },
+
+    moreButton: {
+        width: 65,
+        height: 65,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    moreText: {
+        color: 'black',
+        fontSize: GlobalFontSize.Base,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: -20
+    }
+});
